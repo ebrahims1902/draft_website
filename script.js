e013c0edba65ed0a4dde8e4da4f688dd021c2f92
ebrahim <ebrahim@bilals-MBP.domain.name>
2022-07-22 11:52:22 +0530

    Third Commit

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Analyst", "Developer", "Programmer", "Designer", "Machine Learning Model Predictor"],
+        strings: ["Greatness", "Safety", "Value for money", "Riding Experience", "Style"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
